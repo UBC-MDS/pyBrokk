15ba3ba8d80a3c5fbb9b271b0e605ae15301b223
Elena Ganacheva <elena.ganacheva@gmail.com>
2023-01-20 17:24:36 -0800

    Added pytest to dev tools and added test for duster

--- src/pymine/duster.py
+++ src/pymine/duster.py
@@ -1,42 +1,48 @@
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+from pymine.create_id import create_id
+
 def duster(urls):
     """
-    Counts the occurence of a specific list of words on a series of webpages
+    Prepares a pandas dataframe by webscraping raw text from a list of urls ready to be input into a machine learning model.
     
     Parameters
     ----------
     urls: list
         list of target urls as strings
-    words: list
-        List of target words as strings
-        
+
     Returns
     ----------
-    counts: dataframe
-        A dataframe with the webpage identifiers as a index, target words as columns, and the counts of target words for each webpage
+    df: pandas dataframe
+        A dataframe with the webpage identifiers as a index, the raw url, and the raw text from the webpage with extra line breaks removed.
         
     Examples
     ----------
-    >>> from pymine import count_words
-    >>> count_words(['https://www.cnn.com/world', 'https://www.foxnews.com/world', 'https://www.cbc.ca/news/world'], ['tsunami', 'hurricane', 'earthquake', 'disaster'])
-            tsunami hurricane earthquake disaster)
-    cnn1        0       3           0           1
-    foxnews1    0       6           0           9
-    cbc1        5       1           0           3          
+    >>> from pymine.duster import duster
+    >>> duster(['https://www.cnn.com/world', 'https://www.foxnews.com/world', 'https://www.cbc.ca/news/world'])
+                                        url                                           raw_text
+    id
+    cnn1          https://www.cnn.com/world   World news - breaking news, video, headlines ...
+    foxnews1  https://www.foxnews.com/world  World | Fox NewsFox News   U.S.PoliticsMediaOp...
+    cbc1      https://www.cbc.ca/news/world  World - CBC NewsContentSkip to Main ContentAcc...         
     """
-    import requests
-    from bs4 import BeautifulSoup
-    import pandas as pd
-    from pymine.create_id import create_id
 
     output = {}
     for url in urls:
         page = requests.get(url)
         soup = BeautifulSoup(page.content, "html.parser")
         output[url] = soup.text
-
+    #create Dataframe from dictionary output of text_from_url()
     df = pd.DataFrame.from_dict(output, orient='index', columns=["raw_text"]).reset_index().rename(columns={"index":"url"})
+    
+    #remove line breaks
     df['raw_text'] = df['raw_text'].str.replace("\n", "")
+    
+    #add id as index
     df['id'] = create_id(df['url'].tolist())
+    df = df.set_index('id')
+    
     return df
     
 
---
+++ tests/test_duster.py
@@ -0,0 +1,27 @@
+from pymine.duster import duster
+import pandas as pd
+
+def test_duster():
+    """
+    Test input and output for duster() function
+    
+    Example
+    ---------
+    >>> test_duster()
+    """
+    
+    urls = ['https://www.reddit.com/r/nba/', 'https://www.reddit.com/r/nfl/', 'https://vancouver.craigslist.org/search/apa', 'https://www.kijiji.ca/b-real-estate/richmond-bc/c34l1700288']
+    actual = duster(urls)
+
+    
+    "Confirms the input is a list and all items in the list are strings"
+    assert type(urls) is list and all(isinstance(item, str) for item in urls) == True, "Input is not a list of strings"
+    
+    "Confirms the output is a DataFrame"
+    assert isinstance(actual, pd.DataFrame) == True, "Output is not a DataFrame"
+    
+    "Confirms the output number of rows is correct"
+    assert len(actual) == len(urls), "Output contains an incorrect number of rows."
+    
+    "Confirms the output number of columns is correct"
+    assert len(actual.columns) == 2, "Output contains an incorrect number of columns"
