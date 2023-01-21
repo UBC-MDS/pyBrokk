3a323841baaddc56078596cf50c01fe9d7842a25
Mike Guron <mike_guron@alumni.ubc.ca>
2023-01-20 16:05:42 -0800

    Update function docstring

--- src/pymine/create_id.py
+++ src/pymine/create_id.py
@@ -15,8 +15,8 @@ def create_id(urls):
     Examples
     ----------
     >>> from pymine import create_id
-    >>> create_id(['https://www.reddit.com/r/nba/', 'https://vancouver.craigslist.org/search/apa', 'https://www.kijiji.ca/b-real-estate/richmond-bc/c34l1700288'])
-    ['reddit1', 'craigslist1', 'kijiji1']
+    >>> create_id(['https://www.reddit.com/r/nba/', 'https://www.reddit.com/r/nfl/', 'https://vancouver.craigslist.org/search/apa', 'https://www.kijiji.ca/b-real-estate/richmond-bc/c34l1700288'])
+    ['reddit1', 'reddit2', 'craigslist1', 'kijiji1']
     """
     ids = []
     ids_dict = {}
